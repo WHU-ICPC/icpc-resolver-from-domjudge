76f8500d268841cd5a4c6a75cbbea4964d151ca4
Lanly109 <1094916227@qq.com>
2022-04-27 18:44:57 +0800

    support resolver 2.4

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A tools to generate xml file of icpc-resolver via domjudge RESTful API.
 
 ## Prerequisite
 
-* icpc-resolver == resolver-2.1.2100
+* icpc-resolver >= resolver-2.4
 * domjudge-domserver >= 7.0.1
 * python >= 3.6.8
 
@@ -17,13 +17,30 @@ A tools to generate xml file of icpc-resolver via domjudge RESTful API.
   "key": <Basic Authorization key>,
   "xml": <output xml file name>,
   "showGroupList": [<group1>, <group2>, ...]
+  "gold": <the number of gold medals>,
+  "silver": <the number of silver medals>,
+  "bronze": <the number of bronze medals>,
+  "no_occupy_award_categories": [<group_id1>, <group_id2>, ...],
+  "award_best_girl": [<group_id1>]
 }
 ```
+
+`no_occupy_award_categories`表示给予颁奖，但不占用名额，是想让那些打星选手也亮亮相，而不是没有任何奖项在滚榜时匆匆略过。
+
+默认打星选手不参与一血奖，如需参与则注释225,226行即可。
+
+默认最佳女对奖必须获得牌，如无该条件则注释271,272行即可。
+
 #### example
 ```jsonld
   "url": "https://www.example.com/api/v4/contests/{cid},
   "key": "KEY",
   "xml": "events.xml"
+  "gold": 4,
+  "silver": 4,
+  "bronze": 4,
+  "no_occupy_award_categories": ["18", "20"],
+  "award_best_girl": ["11"]
 ```
 2. run main.py
 ```
--- classes/domjudge.py
+++ classes/domjudge.py
@@ -18,13 +18,14 @@ class DOMjudge:
         req_url = self.config['url'] + method
         print ("[   ] GET %s" % req_url, end='\r')
         req_hdr = { 'Authorization': key }
-        res = requests.get(req_url, headers=req_hdr)
+        res = requests.get(req_url, headers=req_hdr,  verify=False)
         print ("[%d] GET %s" % (res.status_code, req_url))
         return json.loads(res.text)
 
     def load_data(self):
         self.load_contest_info()
         self.load_groups()
+        self.load_organizations()
         self.load_teams()
         self.load_submissions()
         self.load_judgements()
@@ -38,11 +39,20 @@ class DOMjudge:
     def load_groups(self):
         groups = self.API("/groups")
         func = lambda group : not group['hidden']
-        self.groups = list(filter(func, groups))
+        groups = list(filter(func, groups))
+        self.groups = {}
+        for group in groups:
+            self.groups[group['id']] = group
+
+    def load_organizations(self):
+        organizations = self.API("/organizations")
+        self.organizations = {}
+        for organization in organizations:
+            self.organizations[organization['icpc_id']] = organization
 
     def load_teams(self):
         teams = self.API("/teams")
-        group_ids = [group['id'] for group in self.groups]
+        group_ids = [group['id'] for group in self.groups.values()]
         same = lambda x, y: list(set(x) & set(y))
         func = lambda team: len(same(team['group_ids'], group_ids))
         self.teams = list(filter(func, teams))
@@ -121,20 +131,30 @@ class DOMjudge:
     def resolver_contest_formatter(self):
         return {
             'info': self.resolver_info_formatter(),
-            'judgement': self.resolver_judgement_formatter(),
             'problem': self.resolver_problem_formatter(),
+            'region': self.resolver_group_formatter(),
             'team': self.resolver_team_formatter(),
+            'judgement': self.resolver_judgement_formatter(),
             'run': self.resolver_run_formatter(),
             'award': self.resolver_award_formatter(),
             'finalized': self.resolver_finalized_formatter()
         }
+
+    def resolver_group_formatter(self):
+        return [{
+            'external-id': group['id'],
+            'name': group['name']
+        } for group in self.groups.values()]
     
     def resolver_info_formatter(self):
         return {
-            'title': self.contest_info['shortname'],
+            'contest-id': self.contest_info['id'],
+            'title': self.contest_info['name'],
+            'short-title': self.contest_info['shortname'],
             'length': self.contest_info['duration'],
             'scoreboard-freeze-length': self.contest_info['scoreboard_freeze_duration'],
             'starttime': dtime2timestamp(self.contest_info['start_time']),
+            'penalty': self.contest_info['penalty_time'],
         }
 
     def resolver_judgement_formatter(self):
@@ -144,14 +164,21 @@ class DOMjudge:
 
     def resolver_problem_formatter(self):
         return [{ 
-            'id': problem['ordinal'] + 1
+            'id': problem['ordinal'] + 1,
+            'label': problem['label'],
+            'name': problem['name'],
+            # 'color': problem['color'],
+            # 'rgb': problem['rgb'],
         } for problem in self.problems ]
 
     def resolver_team_formatter(self):
         return [{
             'id': team['id'],
+            'external-id': team['icpc_id'],
             'name': escape(team['name']),
-            'university': '中山大学'
+            'university': self.organizations[team['organization_id']]['formal_name'],
+            'university-short-name': self.organizations[team['organization_id']]['shortname'],
+            'region': self.groups[team['group_ids'][0]]['name'],
         } for team in self.teams ]
 
     def resolver_run_formatter(self):
@@ -171,9 +198,10 @@ class DOMjudge:
         return reduce(lambda x, y: x + y, [
             self.resolver_award_first_solved_formatter(),
             self.resolver_award_top_team_formatter(3),
+            self.resolver_award_winner_formatter(),
             self.resolver_award_medal_formatter(),
             self.resolver_award_best_girl_formatter(),
-            self.resolver_award_last_AC()
+            self.resolver_award_last_AC_formatter()
             # self.resolver_award_first_WA()
         ], [])
 
@@ -185,14 +213,11 @@ class DOMjudge:
             'teamId': team_ids
         }
 
-    def get_rank(self, rank):
-        return f"rank-{rank}"
-
     def get_team_categories(self, team_id):
         return self.team_to_categories[team_id][0]
 
     def resolver_award_first_solved_formatter(self):
-        first_solved, first_solved_award = [ False for i in range(len(self.problems)) ], []
+        first_solved, first_solved_award = [ False for _ in range(len(self.problems)) ], []
         problem_id2idx = { problem['id']: problem['ordinal'] for problem in self.problems }
         for submission in self.submissions:
             if not submission['judgement_type']['solved']:
@@ -209,7 +234,7 @@ class DOMjudge:
         return first_solved_award
 
     def resolver_award_top_team_formatter(self, rank):  #WARNING: 排名相同无法一起评
-        buf = [[] for i in range(rank + 1)]
+        buf = [[] for _ in range(rank + 1)]
         cnt = 0
         for row in self.scoreboard['rows']:
             if cnt == rank:
@@ -220,20 +245,34 @@ class DOMjudge:
             buf[cnt].append(row['team_id'])
         top_team_award = []
         for idx, team_ids in enumerate(buf):
-            top_team_award.append(self.award(self.get_rank(idx), '%s Place' % make_ordinal(idx), team_ids))
+            top_team_award.append(self.award(f'rank-{idx}', '%s Place' % make_ordinal(idx), team_ids))
         return top_team_award
+
+    def resolver_award_winner_formatter(self):  #WARNING: 排名相同无法一起评
+        rank = 1
+        buf = [[] for _ in range(rank + 1)]
+        cnt = 0
+        for row in self.scoreboard['rows']:
+            if cnt == rank:
+                break
+            cnt += 1
+            buf[cnt].append(row['team_id'])
+        winner_award = []
+        for _, team_ids in enumerate(buf):
+            winner_award.append(self.award(f'winner', 'World Champion', team_ids))
+        return winner_award
 
     def resolver_award_best_girl_formatter(self):
         best_girls_team_id = -1
         for row in self.scoreboard['rows']:
-            if self.get_team_categories(row['team_id']) in self.config['award_best_girl'] and row['rank'] <= self.limited:
+            if self.get_team_categories(row['team_id']) in self.config['award_best_girl']:
                 best_girls_team_id = row['team_id']
                 break
-            if row['rank'] > self.limited:
+            if row['rank'] > self.limited: # 限定最佳女队必得奖牌
                 break
         best_girls_award = []    
         if best_girls_team_id != -1:
-            best_girls_award.append(self.award(f"group-highlight-{self.config['award_best_girl'][0]}", "The Best Girls's Team", best_girls_team_id))
+            best_girls_award.append(self.award(f"group-winner-{self.config['award_best_girl'][0]}", "The Best Girls's Team", best_girls_team_id))
         return best_girls_award
 
     def resolver_award_medal_formatter(self):
@@ -275,7 +314,7 @@ class DOMjudge:
         self.limited = pos - 1
         return medal_team_award
 
-    def resolver_award_last_AC(self):
+    def resolver_award_last_AC_formatter(self):
         submissions = list(filter(lambda submission: submission['judgement_type']['id'] == "AC", self.submissions))
         if len(submissions) == 0:
             return []
